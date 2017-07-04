d19e3c2e7c1b149ed79cc0c0f25809bf46b002de
Anthony Perry <atperry7@gmail.com>
2017-07-03 19:28:08 -0500

    Refactored current code to be more functional

--- 2-basic-browser-javascript/main.js
+++ 2-basic-browser-javascript/main.js
@@ -1,26 +1,33 @@
 $(document).ready(function() {
   'user strict'
-
   const addition = (num, num2) => num + num2
   const multiply = (num, num2) => num * num2
   const subtraction = (num, num2) => num - num2
 
   const updateDisplay = func => $('.total').html(func)
-
   const getCurrentTotal = () => Number($('.total').html())
-
   const getCurrentMultiplier = () => Number($('.currentMultiplier').html())
+  const setMultiplierState = () => {
+    if (getCurrentTotal() < 10) {
+      $('.leftButton').css('cursor', 'not-allowed')
+    } else {
+      $('.leftButton').css('cursor', 'pointer')
+    }
+  }
+
+  setMultiplierState()
 
   $('.leftButton').click(function() {
-    if (Number($('.total').html()) < 10) {
-      console.log(getCurrentTotal())
-    } else {
+    if (Number($('.total').html()) >= 10) {
       updateDisplay(subtraction(getCurrentTotal(), 10))
       $('.currentMultiplier').html(multiply(getCurrentMultiplier(), 1.2))
     }
+
+    setMultiplierState()
   })
 
   $('.button').click(function() {
     updateDisplay(addition(getCurrentTotal(), getCurrentMultiplier()))
+    setMultiplierState()
   })
 })
